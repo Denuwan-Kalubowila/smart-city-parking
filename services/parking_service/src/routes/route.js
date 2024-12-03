e803e33cd1c07cdebf88f4f8e875812e8c69da4f
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-12-03 17:17:19 +0530

    add prking routes

---
+++ services/parking_service/src/routes/route.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const router = express.Router();
+
+const parkingController = require('../controllers/parking_controller');
+
+router.get("/spot",parkingController.getSpot);
+router.get("spot/:id",parkingController.getSpotById);
+router.post("/spot",parkingController.createSpot);
+router.put("/spot/:id",parkingController.updateSpot);
+router.delete("/spot/:id",parkingController.deleteSpot);
+router.get("/spot/status/avalable",parkingController.availableSpots);
